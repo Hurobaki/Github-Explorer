7f3d180fc4e87fa19e08b9c55dd95846568054ae
Théo HERVEUX <hurobaki@gmail.com>
2019-07-20 09:03:17 +0200

    Initial Commit

---
+++ Github Explorer/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Github Explorer
+//
+//  Created by THE3796 on 20/07/2019.
+//  Copyright © 2019 Firelabs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
